edf1ce2c718c8732120f4cb5af4a5d76347fe679
drfailer <drfailer.42@gmail.com>
2024-03-29 15:36:10 +0100

    test access to database

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 mod scheduler;
+mod schema;
+mod games_repository;
+mod models;
 
 use std::io;
-use actix_web::{ App,  HttpServer };
+use actix_web::{ web, App, HttpServer };
+use diesel::SqliteConnection;
+use diesel::r2d2;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
+    let manager = r2d2::ConnectionManager::<SqliteConnection>::new("app.db");
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("database URL should be valid path to SQLite DB file");
+
+    HttpServer::new(move || {
         App::new()
-            .service(scheduler::test)
+            .app_data(web::Data::new(pool.clone()))
+            .service(scheduler::test_get)
+            .service(scheduler::test_get_game)
+            .service(scheduler::test_post)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,6 +1,43 @@
-use actix_web::{get, HttpResponse, Responder };
+use actix_web::error;
+use actix_web::{get, post, web, HttpResponse, Responder};
+use diesel::r2d2;
+use diesel::SqliteConnection;
+
+use crate::games_repository::{ insert_game, get_game };
+
+type DbPool = r2d2::Pool<r2d2::ConnectionManager<SqliteConnection>>;
 
 #[get("/scheduler/test")]
-pub async fn test() -> impl Responder {
+pub async fn test_get() -> impl Responder {
     HttpResponse::Ok().body("test")
+}
+
+#[get("/scheduler/test/{id}")]
+pub async fn test_get_game(
+    pool: web::Data<DbPool>,
+    id: web::Path<(String,)>,
+) -> actix_web::Result<impl Responder> {
+    let (id,) = id.into_inner();
+    let game = web::block(move || {
+        let mut conn = pool.get().expect("Error: can't connect to db");
+        get_game(&mut conn, id)
+    })
+    .await?
+    .map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok().json(game))
+}
+
+#[post("/scheduler/test/{name}")]
+pub async fn test_post(
+    pool: web::Data<DbPool>,
+    name: web::Path<(String,)>,
+) -> actix_web::Result<impl Responder> {
+    let (name,) = name.into_inner();
+    let game = web::block(move || {
+        let mut conn = pool.get().expect("Error: can't connect to db");
+        insert_game(&mut conn, name)
+    })
+    .await?
+    .map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok().json(game))
 }
