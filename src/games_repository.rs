891efb2792b868eadd018d80b5e7ddfee7422713
drfailer <drfailer.42@gmail.com>
2024-03-29 15:35:49 +0100

    game repository
    todo: create a dal directory

---
+++ src/games_repository.rs
@@ -0,0 +1,29 @@
+use crate::models::Game;
+use diesel::{QueryDsl, RunQueryDsl, SqliteConnection};
+
+pub fn insert_game(conn: &mut SqliteConnection, game_name: String) -> diesel::QueryResult<Game> {
+    use crate::schema::games::dsl::*;
+
+    // Create insertion model
+    let uid = format!("{}", uuid::Uuid::new_v4());
+    let game = Game {
+        id: uid.to_string(),
+        schedule_time: "".to_string(),
+        end_time: "".to_string(),
+        name: game_name,
+        gm_discord_name: "1234".to_string(),
+        gm_discord_id: "1234".to_string(),
+    };
+
+    // normal diesel operations
+    diesel::insert_into(games)
+        .values(&game)
+        .execute(conn)
+        .expect("Error inserting person");
+    games.find(uid).first(conn)
+}
+
+pub fn get_game(conn: &mut SqliteConnection, game_id: String) -> diesel::QueryResult<Game> {
+    use crate::schema::games::dsl::*;
+    games.find(game_id).first(conn)
+}
